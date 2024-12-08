0a13d98d23a6f1ce5734a6e8bb3a92cbfd03da9c
Maryna K <110280285+Omenat83@users.noreply.github.com>
2024-12-08 22:09:56 +0200

    feat

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -470,8 +470,11 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((num) => {
+    const hex = num.toString(16).toUpperCase();
+    return `#${hex.padStart(6, '0')}`;
+  });
 }
 
 /**
@@ -488,8 +491,13 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (n === 0 || arr.length === 0) return [];
+  const result = arr
+    .slice()
+    .sort((a, b) => b - a)
+    .slice(0, n);
+  return result;
 }
 
 /**
@@ -504,8 +512,9 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const result = arr1.filter((el) => arr2.includes(el));
+  return result;
 }
 
 /**
